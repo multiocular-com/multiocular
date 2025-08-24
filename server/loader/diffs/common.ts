b2247d88579cd0cedb68a0963035fa36f9eaced4
Andrey Sitnik <andrey@sitnik.ru>
2025-08-24 23:20:56 +0000

    Add docs to version/diff loading API

--- server/loader/diffs/common.ts
+++ server/loader/diffs/common.ts
@@ -1,5 +1,8 @@
 import type { Change, Diff } from '../../types.ts'
 
+/**
+ * Load dependency diff from version changes.
+ */
 export interface DiffLoader {
   (version: Change): Promise<Diff>
 }
--- server/loader/versions/common.ts
+++ server/loader/versions/common.ts
@@ -1,6 +1,19 @@
 import type { Dependency, FilePath, LoadedFile } from '../../types.ts'
 
+/**
+ * Load versions list from changed files.
+ */
 export interface VersionsLoader {
+  /**
+   * Select files with versions (like lock files) from changed files.
+   *
+   * @param changed Changed files in commit or uncommited files.
+   */
   findFiles(changed: FilePath[]): FilePath[]
+  /**
+   * Extract versions from file content.
+   *
+   * @param files Files selected at {@link VersionsLoader#findFiles}.
+   */
   load(files: LoadedFile[]): Dependency[]
 }
