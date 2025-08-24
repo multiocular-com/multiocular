dcf5d939ab03698593f0009e8a77d4c03b52cf71
Andrey Sitnik <andrey@sitnik.ru>
2025-08-24 16:27:38 +0000

    Add types for CLI json output

--- server/cli/output.ts
+++ server/cli/output.ts
@@ -1,13 +1,17 @@
 import { $diffs, $step } from '../loader/stores.ts'
+import type { ChangeDiff } from '../types.ts'
 import type { Config } from './args.ts'
 import { print } from './print.ts'
+
+export type MultiocularJSON = ChangeDiff[]
 
 export function outputProcess(config: Config): void {
   let unbindStep = $step.listen(step => {
     if (step === 'done') {
       unbindStep()
       if (config.output === 'json') {
-        print(JSON.stringify($diffs.get(), null, 2))
+        let json = $diffs.get() satisfies MultiocularJSON
+        print(JSON.stringify(json, null, 2))
       } else {
         for (let diff of $diffs.get()) {
           print(diff.diff)
