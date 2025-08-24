a8b6a00d8cc03990a8bf2f1931089073fca58e01
Andrey Sitnik <andrey@sitnik.ru>
2025-08-24 20:33:53 +0000

    Do not use branded string for external types

--- server/cli/output.ts
+++ server/cli/output.ts
@@ -1,9 +1,10 @@
+import type { Debrand } from '../../utils/types.ts'
 import { $diffs, $step } from '../loader/stores.ts'
 import type { ChangeDiff } from '../types.ts'
 import type { Config } from './args.ts'
 import { print } from './print.ts'
 
-export type MultiocularJSON = Omit<ChangeDiff, 'id'>[]
+export type MultiocularJSON = Debrand<Omit<ChangeDiff, 'id'>[]>
 
 export function outputProcess(config: Config): void {
   let unbindStep = $step.listen(step => {
