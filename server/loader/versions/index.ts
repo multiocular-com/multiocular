c8f4aa5dc71e252c564ad86092b537c8eedf09d3
Andrey Sitnik <andrey@sitnik.ru>
2025-08-26 19:41:49 +0000

    Add Yarn 1/berry support

--- server/loader/versions/index.ts
+++ server/loader/versions/index.ts
@@ -1,8 +1,9 @@
 import type { VersionsLoader } from './common.ts'
 import { npm } from './npm.ts'
 import { pnpm } from './pnpm.ts'
+import { yarn } from './yarn.ts'
 
-export const versionsLoaders = { npm, pnpm } satisfies Record<
+export const versionsLoaders = { npm, pnpm, yarn } satisfies Record<
   string,
   VersionsLoader
 >
--- server/test/npm.test.ts
+++ server/test/npm.test.ts
@@ -75,6 +75,67 @@ test('shows dependency changes with npm', async () => {
   ])
 })
 
+test('shows dependency changes with yarn 1', async () => {
+  await run('npm install yarn')
+  await run('npx yarn add nanoid@5.1.4')
+  await run('git add .')
+  await run('git commit -m "Add nanoid"')
+  await run('npx yarn add nanoid@5.1.5')
+
+  await cliJsonEqual([
+    {
+      after: '5.1.5',
+      before: '5.1.4',
+      diff:
+        'diff --git npm:nanoid@5.1.4/package.json npm:nanoid@5.1.5/package.json\n' +
+        'index v5.1.4..v5.1.5 100644\n' +
+        '--- npm:nanoid@5.1.4/package.json\n' +
+        '+++ npm:nanoid@5.1.5/package.json\n' +
+        '@@ -1,6 +1,6 @@\n' +
+        ' {\n' +
+        '   "name": "nanoid",\n' +
+        '-  "version": "5.1.4",\n' +
+        '+  "version": "5.1.5",\n' +
+        '   "description": "A tiny (118 bytes), secure URL-friendly unique string ID generator",\n' +
+        '   "keywords": [\n' +
+        '     "uuid",',
+      name: 'nanoid',
+      type: 'npm'
+    }
+  ])
+})
+
+test('shows dependency changes with yarn berry', async () => {
+  await run('npm install yarn')
+  await run('npx yarn init -2')
+  await run('npx yarn add nanoid@5.1.4')
+  await run('git add .')
+  await run('git commit -m "Add nanoid"')
+  await run('npx yarn add nanoid@5.1.5')
+
+  await cliJsonEqual([
+    {
+      after: '5.1.5',
+      before: '5.1.4',
+      diff:
+        'diff --git npm:nanoid@5.1.4/package.json npm:nanoid@5.1.5/package.json\n' +
+        'index v5.1.4..v5.1.5 100644\n' +
+        '--- npm:nanoid@5.1.4/package.json\n' +
+        '+++ npm:nanoid@5.1.5/package.json\n' +
+        '@@ -1,6 +1,6 @@\n' +
+        ' {\n' +
+        '   "name": "nanoid",\n' +
+        '-  "version": "5.1.4",\n' +
+        '+  "version": "5.1.5",\n' +
+        '   "description": "A tiny (118 bytes), secure URL-friendly unique string ID generator",\n' +
+        '   "keywords": [\n' +
+        '     "uuid",',
+      name: 'nanoid',
+      type: 'npm'
+    }
+  ])
+})
+
 test('shows new dependency', async () => {
   await run('pnpm add nanoid@5.1.5')
   await cliJsonMatch([
@@ -164,6 +225,29 @@ test('shows git dependency changes with npm', async () => {
     }
   ])
 })
+
+test('shows git dependency changes with yarn 1', async () => {
+  let beforeCommit = 'c0b7b0c33797d4397310bafe517d7e8b65bbf3cc'
+  let afterCommit = '27ee2c4b80dc6ddf7916b6ec933f462945ddf3bc'
+
+  await run('npm install yarn')
+  await run(`npx yarn add nanoid@ai/nanoid#${beforeCommit}`)
+  await run('git add .')
+  await run('git commit -m "Add nanoid from git"')
+  await run(`npx yarn add nanoid@ai/nanoid#${afterCommit}`)
+
+  await cliJsonMatch([
+    {
+      after: new RegExp(afterCommit),
+      before: new RegExp(beforeCommit),
+      name: 'nanoid',
+      type: 'npm'
+    }
+  ])
+})
+
+// No Yarn Berry git dependency test until this issue will be fixed
+// https://github.com/yarnpkg/berry/issues/3169
 
 test('shows nested dependency updates in pnpm', async () => {
   // Force nested nanoid version
