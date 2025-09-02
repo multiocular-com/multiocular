def55f3bbe665ff43d6dac17701907680fb60910
Andrey Sitnik <andrey@sitnik.ru>
2025-09-02 15:50:25 +0000

    Add font

--- web/main/index.ts
+++ web/main/index.ts
@@ -1,5 +1,7 @@
 import './sync.ts'
 
+import './index.css'
+
 import { mount } from 'svelte'
 
 import Main from './main.svelte'
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
   build: {
+    assetsInlineLimit: 0,
     cssMinify: false,
     emptyOutDir: true,
     minify: false,
