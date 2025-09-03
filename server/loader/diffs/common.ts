da7662a27d210f8ac15788e26ab4269170a78b98
Andrey Sitnik <andrey@sitnik.ru>
2025-09-03 08:17:55 +0000

    Remove useless comments

--- server/loader/diffs/common.ts
+++ server/loader/diffs/common.ts
@@ -1,15 +1,9 @@
 import type { Change, Diff } from '../../../common/types.ts'
 
-/**
- * Load dependency diff from version changes.
- */
 export interface DiffLoader {
   (version: Change): Promise<Diff>
 }
 
-/**
- * Generate diff prefixes for libnpmdiff based on change information
- */
 export function getDiffPrefixes(change: Change): {
   diffDstPrefix: string
   diffSrcPrefix: string
