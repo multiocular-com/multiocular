a719e9c4b87ebbc9b40c94b050ee6af5fa25b423
Andrey Sitnik <andrey@sitnik.ru>
2025-10-09 08:55:25 +0000

    Fix version changes on Chromatic

--- web/.storybook/preview.ts
+++ web/.storybook/preview.ts
@@ -4,12 +4,6 @@ import { withThemeByClassName } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/svelte'
 import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from 'storybook/viewport'
 
-// @ts-expect-error Emulating Vite define
-globalThis.__SERVER_URL__ = 'offline'
-
-// @ts-expect-error Prevent stories update on version bump
-globalThis.__MULTIOCULAR_VERSION__ = '0.0.0'
-
 export default {
   decorators: [
     withThemeByClassName({
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,19 +4,26 @@ import { defineConfig } from 'vite'
 
 import { getVersion } from '../server/index.ts'
 
-export default defineConfig(({ command }) => ({
-  build: {
-    assetsInlineLimit: 0,
-    cssMinify: false,
-    emptyOutDir: true,
-    minify: false,
-    outDir: join(import.meta.dirname, '..', 'dist', 'web')
-  },
-  define: {
-    __MULTIOCULAR_VERSION__: JSON.stringify(getVersion()),
-    __SERVER_URL__: JSON.stringify(
-      command === 'build' ? '' : 'ws://localhost:31337'
-    )
-  },
-  plugins: [svelte()]
-}))
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      assetsInlineLimit: 0,
+      cssMinify: false,
+      emptyOutDir: true,
+      minify: false,
+      outDir: join(import.meta.dirname, '..', 'dist', 'web')
+    },
+    define: process.env.STORYBOOK
+      ? {
+          __MULTIOCULAR_VERSION__: '"0.0.0"',
+          __SERVER_URL__: '"offline"'
+        }
+      : {
+          __MULTIOCULAR_VERSION__: JSON.stringify(getVersion()),
+          __SERVER_URL__: JSON.stringify(
+            command === 'build' ? '' : 'ws://localhost:31337'
+          )
+        },
+    plugins: [svelte()]
+  }
+})
